2ec5f466f0fdd91da4ebd891d05353187717973c
Ben <ask@benstheory.com>
2024-01-26 12:41:18 +0530

    m

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,9 +6,5 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index()
-    {
-        $users = User::all();
-        return response()->json(['users' => $users], 200);
-    }
+
 }
