aaa734edf4534255dfa2ecea63f5fbbbcac2a905
Ben <ask@benstheory.com>
2024-01-27 07:40:20 +0530

    m

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/users', [UserController::class, 'index'])->name('api.users.index');
+Route::get('users', [UserController::class, 'index'])->name('api.users.index');
