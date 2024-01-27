3efd076d43d920aba3d7bc60e9f3b2e512f3b7ac
Ben <ask@benstheory.com>
2024-01-27 23:29:52 +0530

    push

--- app/Http/Controllers/postAPI.php
+++ app/Http/Controllers/postAPI.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\leads;
 use Illuminate\Http\Request;
 
 class postAPI extends Controller
 {
     //
+    function getMethod()
+    {
+        return leads::all();
+    }
 }
