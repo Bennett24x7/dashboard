fea51dcc954c3e0fa4eefc6acb54fa0070b19e97
Ben <ask@benstheory.com>
2024-01-27 13:17:30 +0530

    push

--- app/Http/Controllers/dummyAPI.php
+++ app/Http/Controllers/dummyAPI.php
@@ -9,18 +9,22 @@ class dummyAPI extends Controller
 {
     function getData(Request $req)
     {
-        $leads = new leads;
-        $leads->fname=$req->fname;
-        $leads->lname=$req->lname;
-        $leads->email=$req->email;
-        $leads->phone=$req->phone;
-        $result = $leads->save();
-        if($result){
-            return ["result"=>"data has been saved"];
+        // Basic validation to check if required fields are not empty
+        if (empty($req->fname) || empty($req->email)) {
+            return ["result" => "Failed: Required fields are empty"];
         }
-        else
-        {
-            return ["result"=>"failed"];
+
+        $leads = new leads;
+        $leads->fname = $req->fname;
+        $leads->lname = $req->lname;
+        $leads->email = $req->email;
+        $leads->phone = $req->phone;
+
+        // Attempt to save the data
+        if ($leads->save()) {
+            return ["result" => "Data has been saved"];
+        } else {
+            return ["result" => "Failed to save data"];
         }
     }
 }
