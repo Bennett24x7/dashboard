4ae2f3b66c7b9629891ae1c2bc61e685b110cd8f
Ben <ask@benstheory.com>
2024-01-27 12:54:50 +0530

    push

--- app/Http/Controllers/dummyAPI.php
+++ app/Http/Controllers/dummyAPI.php
@@ -10,10 +10,10 @@ class dummyAPI extends Controller
     function getData(Request $req)
     {
         $leads = new leads;
-        $leads->fname=$req->fname;
-        $leads->lname=$req->lname;
-        $leads->email=$req->email;
-        $leads->phone=$req->phone;
+        $leads->Field_1=$req->fname;
+        $leads->Field_2=$req->lname;
+        $leads->Field_3=$req->email;
+        $leads->Field_4=$req->phone;
         $result = $leads->save();
         if($result){
             return ["result"=>"data has been saved"];
