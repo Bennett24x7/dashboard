c6388d8d32e13f1fc4b97a3aebc04904e5a6f4ed
Ben <ask@benstheory.com>
2024-02-01 12:34:20 +0530

    dummyAPI edit

--- app/Http/Controllers/dummyAPI.php
+++ app/Http/Controllers/dummyAPI.php
@@ -71,6 +71,11 @@ class dummyAPI extends Controller
 
     function list()
     {
-        return leads::all();
+        $leads = Leads::select('name','email','phone','address','website','linkedin','country','native_english_speaker','skill',
+        'other_skill','experience','subject_speciality','other_subject_speciality','genre','other_genre','education','specialization',
+        'non_english','french','spanish','italian','japanese','chinease','dutch','german','special_instruction','work_handled',
+        'publisher_vendor','format_handle','other_format','currency','other_currency','testimony','file_upload')
+        -> get();
+        return response()->json($leads);
     }
 }
